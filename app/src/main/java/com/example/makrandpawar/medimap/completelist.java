5aa339852da46bb311606abceac38bb4cee01287
ColdSpike <makrandpawar96@gmail.com>
2016-10-03 16:20:11 +0530

    Recycler view

--- app/src/main/java/com/example/makrandpawar/medimap/completelist.java
+++ app/src/main/java/com/example/makrandpawar/medimap/completelist.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class completelist extends AppCompatActivity {
 
     @Override
@@ -12,6 +19,26 @@ public class completelist extends AppCompatActivity {
         setContentView(R.layout.activity_completelist);
 
         Intent intent = getIntent();
-        String med = intent.getStringExtra(MainActivity.medicinename);
+        final String med = intent.getStringExtra(MainActivity.medicinename);
+
+
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://medimap-fbbb5.firebaseio.com/");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(med).exists()){
+                    final Query query = databaseReference.child(med).orderByChild("value").startAt(1);
+
+                }
+                else {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
